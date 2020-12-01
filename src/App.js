9b9006ddc0112f274962d4af00b08dd65e13a698
DESKTOP-SQA3D42\jh <tla149@daum.net>
2020-12-02 02:19:30 +0900

    그래서 트랜잭션 해쉬가 뭔데요....

--- src/App.js
+++ src/App.js
@@ -50,10 +50,35 @@ const App = () => {
         , gas: 470000
     })
     .then((result) => {
-      console.log(result);
+       console.log(result);
+      // 그래서 트랜잭션 해쉬값으로 뭘...
+      console.log(web3.utils.hexToAscii(web3.utils.toHex(result.transactionHash)));
+      console.log(typeof(result.transactionHash));
+//      console.log(web3.utils.hexToString(result.transactionHash));      
     });
   }
 
+  function toHex(s) {
+    let h = ''
+    for (let i = s.length - 1; i >= 0; i--)
+        h = '%'+ s.charCodeAt(i).toString(16) + h
+    return h
+  }
+  function totalSupply(){ 
+
+    contract
+    .methods
+    .totalSupply()
+    .send({
+        from:      "0xDA6C4962518C3b381103ca6FfF16C35bb7E633E3"
+        , gas: 470000
+    })
+    .then((result) => {
+      console.log(result);
+      console.log(web3.utils.hexToString(result.transactionHash));
+    });
+
+  }
   const onClick= (event) => {
     event.preventDefault();
     const whatDo = event.target.value;
@@ -61,6 +86,8 @@ const App = () => {
       mint();
     }else if(whatDo == 'balanceOf'){
       balanceOf();
+    }else if(whatDo == "totalSupply"){
+      totalSupply();
     }
 
     /*
